13002761c4f673438c7bbf4c2674a8f2d2c15a1c
Kent Anderson <kent@kentanderson.com>
2016-02-07 22:44:12 -0800

    sort

--- app/helpers/packs_helper.rb
+++ app/helpers/packs_helper.rb
@@ -1,6 +1,6 @@
 module PacksHelper
 	def pack_name_event(event_id)
-	  pack_where = Pack.where("event_id" => event_id, "user_id" => current_user.id)# lookup pack_id 
+	  pack_where = Pack.where("event_id" => event_id, "user_id" => current_user.id)	# lookup pack_id 
 
 	  if pack_where.exists? then													# This is where the @pack is created/accessed
 	  	Pack.find(pack_where.first.id).name
