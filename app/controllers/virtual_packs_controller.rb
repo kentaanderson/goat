5aa5a69ef52578da926560a410061def4f244edc
Kent Anderson <kent@kentanderson.com>
2016-04-13 00:46:00 -0700

    problem with the featured view

--- app/controllers/virtual_packs_controller.rb
+++ app/controllers/virtual_packs_controller.rb
@@ -7,14 +7,14 @@ class VirtualPacksController < ApplicationController
 
 	pack_id = params[:vp]
 	@pack = Pack.find(pack_id)
-	if @pack.user_id == current_user.id then
+#	if @pack.user_id == current_user.id then
 	  # make this automatic with model associations then you can call "@pack.event..."
 	  @user_id = @pack.user_id
 	  @event = Event.find(@pack.event_id)
 	  @categories = Category.all
-	else
-  	  redirect_to home_path
-	end
+#	else
+# 	  redirect_to home_path
+#	end
 
   end
 
